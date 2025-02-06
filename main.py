efee31c2c228bb96c696c3c9e4f05a1a3dce0250
Baldan661 <telmnsmurf@gmail.com>
2025-02-07 04:09:22 +0800

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from Test import Test
+from test import Test
 
 __CHANNEL_USERNAME__ = "TelmunnTest"
 __GROUP_USERNAME__   = "TelmunnTestChat"
